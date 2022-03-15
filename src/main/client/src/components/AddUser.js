f86b5dd79dc325e05743a5e7c7843bf9b3188995
akash <akas25n@gmail.com>
2022-03-15 23:01:29 +0100

    initial commit

---
+++ src/main/client/src/components/AddUser.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AddUser = () => {
+    return <div className="flex max-w-2xl">
+        <div className= "px-8 py-8 shadow">
+            <div className="">
+                <h1>Add New User</h1>
+            </div>
+        </div>
+    </div>
+}
+
+export default AddUser;
